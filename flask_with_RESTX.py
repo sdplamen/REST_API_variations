ea9e0b33c491dc0ffbfbb6dd0b4a183c8b71ba1a
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2023-04-14 11:45:16 +0300

    Create flask_with_RESTX.py

---
+++ flask_with_RESTX.py
@@ -0,0 +1,7 @@
+"""
+This is a sample RESTful API implementation with plain Flask but we will use on top a module called Flask RestX
+https://flask-restx.readthedocs.io/en/latest/
+"""
+
+from flask import Flask
+from flask_with_restx import Api, Resource
