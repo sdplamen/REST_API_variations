2fbfd2b6427aa1c33c57ab6067a4e9e679cd497e
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2023-04-25 21:06:25 +0300

    Update FastAPI.py

--- FastAPI.py
+++ FastAPI.py
@@ -1,27 +1,35 @@
 from typing import Union
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
 
+books = [
+    {'id': 1, 'name': 'Pod Igoto', 'author': 'Ivan Vazov'},
+    {'id': 2, 'name': 'Python for Dummies', 'author': 'Stef Maruch'}
+]
 
-class Item(BaseModel):
+class Book(BaseModel):
+    id: int
     name: str
-    price: float
-    is_offer: Union[bool, None] = None
+    author: str
+
+@app.get("/books")
+def get_books():
+    return books
+
+@app.post("/books")
+def create_books(new_book: Book):
+    books.append(new_book.dict())
+    return new_book
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.put("/book/{book_id}")
+def update_book(book_id: int, update_book: Book):
+    for el in books:
+        if el.get('id') == book_id:
+            el['name'] = update_book.name
+            el['author'] = update_book.author
+            return {'book_updated': update_book.dict()}
+    else:
+        return {'book_updated': 'not found'}
